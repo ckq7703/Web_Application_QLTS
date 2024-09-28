03220eb9f1504ba032889af87770e4274cad7f60
ckq7703 <ckq7703@gmail.com>
2024-09-28 09:14:45 +0700

    Dự Án Web Application Quản lý Tài Sản

---
+++ WebApplication_CaoKienQuoc/Controllers/BooksController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_CaoKienQuoc.Models;
+
+namespace WebApplication_CaoKienQuoc.Controllers
+{
+    public class BooksController : Controller
+    {
+        private List<Book> listBooks;
+
+        public BooksController()
+        {
+            listBooks = new List<Book>();
+            listBooks.Add(new Book()
+            {
+                Id = 1,
+                Title = "Sach 1",
+                Author = "Tac gia sach 1",
+                PublicYear = 2817,
+                Price = 40000,
+                Cover = "https://th.bing.com/th/id/OIP.JRE8gYBuVHguNRKqnGsHRAAAAA?rs=1&pid=ImgDetMain"
+            });
+            listBooks.Add(new Book()
+            {
+                Id = 2,
+                Title = "Sach 2",
+                Author = "Tac gia sach 2",
+                PublicYear = 2817,
+                Price = 40000,
+                Cover = "https://th.bing.com/th/id/OIP.JRE8gYBuVHguNRKqnGsHRAAAAA?rs=1&pid=ImgDetMain"
+            });
+            listBooks.Add(new Book()
+            {
+                Id = 3,
+                Title = "Sach 3",
+                Author = "Tac gia sach 3",
+                PublicYear = 2817,
+                Price = 40000,
+                Cover = "https://th.bing.com/th/id/OIP.JRE8gYBuVHguNRKqnGsHRAAAAA?rs=1&pid=ImgDetMain"
+            });
+        }
+
+        // GET: Books
+        public ActionResult ListBooks()
+        {
+            ViewBag.TitlePageName = "Book view page";
+            return View(listBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Book book = listBooks.Find(s => s.Id == id);
+
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(book);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Book book = listBooks.Find(s => s.Id == id);
+
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var editBook = listBooks.Find(b => b.Id == book.Id);
+                    editBook.Title = book.Title;
+                    editBook.Author = book.Author;
+                    editBook.Cover = book.Cover;
+                    editBook.Price = book.Price;
+                    editBook.PublicYear = book.PublicYear;
+
+                    // Return to the list view after successful edit
+                    return View("ListBooks", listBooks);
+                }
+                catch (Exception ex)
+                {
+                    // Log exception if needed
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                // Add model level error message
+                ModelState.AddModelError("", "Input Model Not Valid!");
+
+                // Return to the edit view with model errors
+                return View(book);
+            }
+        }
+
+    }
+}
---
+++ WebApplication_CaoKienQuoc/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_CaoKienQuoc.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        [StringLength(50, ErrorMessage = "Author less than 50 characters")]
+        public string Author { get; set; }
+        public int PublicYear { get; set; }
+        public double Price { get; set; }
+        public string Cover { get; set; }
+
+    }
+}
